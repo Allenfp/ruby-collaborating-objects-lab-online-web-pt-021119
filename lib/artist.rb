df5813a965b1abdeb846f0b53179886e52e874e5
Allen F Partlow <allenfp@gmail.com>
2019-03-24 03:25:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,10 +23,10 @@ class Artist
     @@all << self
   end
 
-  def self.find_or_create_by_name(artist_name)
-  self.new(artist_name) unless all.map do |x|
-    x.name.include? (artist_name)
-  end
+  # def self.find_or_create_by_name(artist_name)
+  # self.new(artist_name) unless all.map do |x|
+  #   x.name.include? (artist_name)
+  # end
   end
 
 end
