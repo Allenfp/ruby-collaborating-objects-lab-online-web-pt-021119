b8c485c910e3392b5ab0345b0a22eae053754b3b
Allen F Partlow <allenfp@gmail.com>
2019-03-24 03:39:07 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,6 +8,6 @@ class MP3Importer
 
   def files
     directory = Dir.entries(@path+"/")
-    directory.find {|file_path| file_path.end_with? ".mp3" }
+    directory.collect {|file_path| file_path.end_with? ".mp3" }
   end
 end
