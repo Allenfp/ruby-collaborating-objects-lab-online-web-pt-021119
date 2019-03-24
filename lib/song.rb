bfc83589d1fcacc800d340b042cce544da4eb207
Allen F Partlow <allenfp@gmail.com>
2019-03-24 03:56:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
   def self.new_by_filename(file_name)
     artist, title = file_name.split(" - ")
     s = new(title)
-    s.artist = Artist.new(file_name.split(" - ")[0])
+    s.artist = Artist.new(artist)
     s.artist.save
     s.artist.songs << s.name
     s
