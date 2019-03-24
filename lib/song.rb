9849555e5f247a250485c341cecf6d365ed45a0c
Allen F Partlow <allenfp@gmail.com>
2019-03-24 03:48:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,17 @@ require 'pry'
 
 class Song
 
-  attr_accessor :name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
   end
 
-  def self.new_by_filename(file)
-    artist, title = file.split(" - ")
-    binding.pry
-  end
+
+
+  # def self.new_by_filename(file)
+  #   artist, title = file.split(" - ")
+  #   binding.pry
+  # end
 
 end
